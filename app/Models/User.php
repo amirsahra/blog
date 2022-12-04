7f8576cc3ae4d8e83193ad57673ca552ad5d72a7
amirsahra <amirhosein.sahra@gmail.com>
2022-12-04 10:08:53 +0330

    feat:create province model and migration

--- app/Models/User.php
+++ app/Models/User.php
@@ -41,4 +41,9 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function province()
+    {
+        return $this->hasOne(Province::class);
+    }
 }
