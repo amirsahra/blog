cb239b0ee7661c29a294ba4edb6552002bc8db56
amirsahra <amirhosein.sahra@gmail.com>
2022-12-04 10:29:30 +0330

    feat:create category model and migration

---
+++ app/Models/Category.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Category extends Model
+{
+    use HasFactory;
+
+    protected $fillable=[
+        'name','slug','parent_id'
+    ];
+
+    public function children()
+    {
+        return $this->hasMany(Category::class,'parent_id','id')
+            ->with('children');
+    }
+}
