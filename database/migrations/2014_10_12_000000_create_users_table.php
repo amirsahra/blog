bed28a31c7c03159b621d025d1177d66e0211624
amirsahra <amirhosein.sahra@gmail.com>
2022-12-04 09:39:39 +0330

    feat:create user migration

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,8 +15,17 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('first_name');
+            $table->string('last_name');
+            $table->string('nationality_code')->unique();
+            $table->string('phone')->unique();
+            $table->time('birthday');
+            $table->enum('gender',['female','male'])->default('male');
+            $table->enum('military',['end','exempt','doing','not_included'])->default('not_included');
             $table->string('email')->unique();
+            $table->string('avatar')->nullable();
+            $table->unsignedBigInteger('province_id');
+            $table->unsignedBigInteger('regen_id');
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
