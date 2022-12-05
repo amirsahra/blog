e04c947c8c4d85917a413f13e0a713649928bd50
amirsahra <amirhosein.sahra@gmail.com>
2022-12-05 11:28:44 +0330

    feat:comment

---
+++ database/factories/CommentFactory.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class CommentFactory extends Factory
+{
+    private static $counter = 0;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'title' => $this->faker->title(),
+            'content' => $this->faker->realTextBetween(50, 100),
+            'post_id' => $this->faker->numberBetween(1,100),
+            'user_id' => $this->faker->numberBetween(1,100),
+        ];
+    }
+}
